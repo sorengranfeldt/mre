92389019e3955b9a581bdb657377e44e38d124f0
Sean McArdle <sean.mcardle0@gmail.com>
2019-01-18 10:39:49 -0800

    Added optional case sensitivity.

--- Model/Condition.cs
+++ Model/Condition.cs
@@ -302,6 +302,7 @@ namespace Granfeldt
     {
         public string MVAttribute;
         public string Pattern;
+        public bool CaseSensitive;
 
         public override bool Met(MVEntry mventry, CSEntry csentry)
         {
@@ -313,8 +314,11 @@ namespace Granfeldt
 
             if (mventry[this.MVAttribute].Values?.Count > 0)
             {
-                var entries = mventry[this.MVAttribute].Values.Cast<string>();
-                return entries.Any(x => String.Equals(x, Pattern, StringComparison.CurrentCultureIgnoreCase));
+                var cmpOptions = StringComparison.CurrentCulture;
+                if (!CaseSensitive) { cmpOptions = StringComparison.CurrentCultureIgnoreCase; }
+
+                var entries = mventry[this.MVAttribute].Values.ToStringArray();
+                return entries.Any(x => String.Equals(x, Pattern, cmpOptions));
             }
             else
             {
